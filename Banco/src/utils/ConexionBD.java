f1c8144248a41959e53af6a16613089e871b721e
Minajio <andres.osoriozapata97@gmail.com>
2024-04-04 14:33:50 -0500

    prueba git

--- Banco/src/utils/ConexionBD.java
+++ Banco/src/utils/ConexionBD.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils;
 
-/**
- *
- * @author andre
- */
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    Connection conexion;
+    public Statement manipular;
     
+    public ConexionBD(){
+        String hostname = "localhost";
+	String puerto = "3306";
+	String databasename = "banco";
+	String user = "root";
+	String password = "";
+        
+        String url = "jdbc:mysql://"+hostname+":"+puerto+"/"+databasename;
+        
+        try {
+	    conexion = DriverManager.getConnection(url, user, password);
+	    manipular = conexion.createStatement();
+	    System.out.println("Conexion exitosa a: "+databasename);
+	} catch (SQLException e) {
+	    System.out.println("Error al conectar con la db: " + e.getMessage());
+	}
+    }
 }
