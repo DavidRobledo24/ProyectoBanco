ae3ad1bae5cf48130d7e0d7dcea0181b9f4b1a8f
DavidRobledo24 <darobledo98@soy.sena.edu.co>
2024-04-04 15:00:34 -0500

    conecxion establecida

--- Banco/src/main/Banco.java
+++ Banco/src/main/Banco.java
@@ -1,11 +1,11 @@
 package main;
 
+import utils.ConexionBD;
 
 public class Banco {
-
     
     public static void main(String[] args) {
-        
+        ConexionBD basedatos = new ConexionBD();
     }
     
 }
