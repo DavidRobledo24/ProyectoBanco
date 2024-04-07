f74a2f64ccdcaf5383c5d9742d83cf9a4cd25f1f
Minajio <andres.osoriozapata97@gmail.com>
2024-04-06 22:13:07 -0500

    Inicio de sesion funcionalidad

--- Banco/src/main/Banco.java
+++ Banco/src/main/Banco.java
@@ -1,11 +1,13 @@
 package main;
 
+import interfaces.InicioSesion;
 import utils.ConexionBD;
 
 public class Banco {
     
     public static void main(String[] args) {
         ConexionBD basedatos = new ConexionBD();
+        new InicioSesion(basedatos);
     }
     
 }
