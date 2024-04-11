312e8c72bd8925a5d53d93e7e1ff6b00d284fd7e
DavidRobledo24 <darobledo98@soy.sena.edu.co>
2024-04-11 16:36:14 -0500

    ultimo detalle

--- Banco/src/main/Banco.java
+++ Banco/src/main/Banco.java
@@ -1,11 +1,13 @@
 package main;
 
+import interfaces.MenuGerenteGeneral;
 import utils.ConexionBD;
 
 public class Banco {
     
     public static void main(String[] args) {
         ConexionBD basedatos = new ConexionBD();
+        MenuGerenteGeneral ventana = new MenuGerenteGeneral();
     }
     
 }
