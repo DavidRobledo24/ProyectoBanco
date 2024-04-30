5efe4afb10d14b350c7ab1a2f6e799aa7753497d
DavidRobledo24 <darobledo98@soy.sena.edu.co>
2024-04-30 15:42:06 -0500

    temp

--- Banco/src/interfaces/InicioSesion.java
+++ Banco/src/interfaces/InicioSesion.java
@@ -196,7 +196,7 @@ public class InicioSesion extends javax.swing.JFrame {
                     dispose();
                 }
                 else{
-                    //new MenuVendedorGeneral(database);
+                    new MenuVendedorGeneral(database);
                     dispose();
                 }
             }
